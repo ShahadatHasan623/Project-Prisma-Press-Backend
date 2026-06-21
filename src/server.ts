58da79845435b6aaf2c3f6b20759e40f219a192f
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-06-22 00:49:07 +0600

    added prisma models defenitation and prisma generated

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import { prisma } from "./lib/prisma"
 const PORT =config.port
 const main =async()=>{
        try {
-        // await prisma.$connect()
+        await prisma.$connect()
         console.log("Connected to the database successfully ");
          app.listen(PORT,()=>{
               console.log(`Server is running on port ${PORT}`)
          })
        } catch (error) {
             console.log("Error starting the server:",error);
-            // await prisma.$disconnect()
+            await prisma.$disconnect()
             process.exit(1)
        }
 }
