0fa6a127d3fd4ee06ddbf1f0c37f5dd0092b45fa
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-06-21 21:25:57 +0600

    added all project npm package install done

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app"
+
+const PORT =process.env.PORT || 5000
+const main =()=>{
+       try {
+         app.listen(PORT,()=>{
+              console.log(`Server is running on port ${PORT}`)
+         })
+       } catch (error) {
+            console.log("Error starting the server:",error);
+       }
+}
+main()
