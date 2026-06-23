1a9f726e80ea0322fd9d5301008e4c0f6158543c
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-06-24 00:04:25 +0600

    added jwt access token and refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,18 +3,17 @@ import { sendResponse } from "../../utils/SendResponse";
 import { authService } from "./auth.service";
 import httpStatus from "http-status";
 
+const loginUser = catchAsync(async (req, res, next) => {
+  const payload = req.body;
+  const loginedUser = await authService.loginUserIntoDB(payload);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "User logged in successfully",
+    data: loginedUser,
+  });
+});
 
-const loginUser =catchAsync(async(req,res,next)=>{
-          const payload = req.body;
-          const loginedUser =await authService.loginUserIntoDB(payload);
-          sendResponse(res,{
-                    success:true,
-                    statusCode:httpStatus.OK,
-                    message:"User logged in successfully",
-                    data:loginedUser
-          })
-})
-
-export const authController={
-          loginUser,
-}
+export const authController = {
+  loginUser,
+};
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,25 +1,43 @@
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utils/jwt";
 import { ILoginUser } from "./auth.interface";
 import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
+const loginUserIntoDB = async (payload: ILoginUser) => {
+  const { email, password } = payload;
 
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
 
-const loginUserIntoDB =async(payload:ILoginUser)=>{
-     const {email,password}=payload;
+  const passwordMatch = await bcrypt.compare(password, user.password);
 
-     const user =await prisma.user.findUniqueOrThrow({
-          where:{email}
-     })
+  if (!passwordMatch) {
+    throw new Error("Invalid credentials");
+  }
+  const jwtPayload = {
+    name: user.name,
+    email: user.email,
+    id: user.id,
+    role: user.role,
+  };
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions
+  );
 
-     const passwordMatch =await bcrypt.compare(password,user.password)
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions
+  );
 
-     if(!passwordMatch){
-          throw new Error("Invalid credentials")
-     }
+  return { accessToken, refreshToken, user };
+};
 
-     return user;
-}
-
-export const authService ={
-          loginUserIntoDB
-}
+export const authService = {
+  loginUserIntoDB,
+};
