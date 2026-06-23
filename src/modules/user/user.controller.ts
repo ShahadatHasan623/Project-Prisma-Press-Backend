b0595ffe7069fc9bc43a839e7b61f983389a2e1d
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-06-23 22:12:44 +0600

    added sendResponse dynamically response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,17 +2,18 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/SendResponse";
 
 
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
   const user = await userService.registerUserIntoDB(payload);
-  res.status(httpStatus.CREATED).json({
+  sendResponse(res, {
     success: true,
-    statusbar: httpStatus.CREATED,
-    message: "User created successfully",
-    data: { user }
+    statusCode: httpStatus.CREATED,
+    message: "User registered successfully",
+    data: user,
   });
 });
 
