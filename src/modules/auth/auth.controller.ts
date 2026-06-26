63a22c976a921f24d03c47d4e359feea2e8f483f
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-06-27 00:11:00 +0600

    added refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/SendResponse";
 import { authService } from "./auth.service";
@@ -31,6 +32,27 @@ const loginUser = catchAsync(async (req, res, next) => {
   });
 });
 
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const { accessToken } = await authService.refreshToken(refreshToken);
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, // 1day
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Token Refresh Succesfully",
+      data: { accessToken },
+    });
+  }
+);
 export const authController = {
   loginUser,
+  refreshToken,
 };
